8b644cfdc022380e61fb6961384a51ef188190f6
mbnaj <mbnaj@hotmail.com>
2022-04-26 14:03:03 +0100

    error handling

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const { getUsers, joinUser, getUserById, logoutUser } = require("./user");
 app.use(express.json());
 app.use(cors());
 
-
 const port = process.env.PORT || 8000;
-
 
 app.get("/rooms", (req, res) => {
   const rooms = getRooms();
@@ -29,8 +27,6 @@ var server = app.listen(port, () => {
 const io = socket(server);
 
 io.on("connection", (socket) => {
-
-
   socket.on("join", ({ username, room_id }) => {
     const user = joinUser(socket.id, username, room_id);
     const room = getRoomById(room_id);
@@ -42,7 +38,7 @@ io.on("connection", (socket) => {
       user_id: user.user_id,
       username: user.username,
       text: `Welcome ${user.username} to room ${room.name}`,
-      room:room.name,
+      room: room.name,
     });
 
     socket.broadcast.to(room_id).emit("message", {
@@ -54,13 +50,14 @@ io.on("connection", (socket) => {
 
   socket.on("sendMessage", (text) => {
     const user = getUserById(socket.id);
-
-    io.to(user.room_id).emit("message", {
-      user_id: user.id,
-      username: user.username,
-      text: text,
-    });
-    console.log(`User ${user.username} sent a message ${text}`);
+    if (typeof(user.room_id)!== 'undefined') {
+      io.to(user.room_id).emit("message", {
+        user_id: user.id,
+        username: user.username,
+        text: text,
+      });
+      console.log(`User ${user.username} sent a message ${text}`);
+    }
   });
 
   socket.on("disconnect", () => {
